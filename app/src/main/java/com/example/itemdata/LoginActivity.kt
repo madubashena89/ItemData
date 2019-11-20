a95330a942ac9d601583b7744a597bd5f5b24103
ASUS <madubashena89@gmail.com>
2019-11-19 23:12:27 -0600

    Login page UI

---
+++ app/src/main/java/com/example/itemdata/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.itemdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/itemdata/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.itemdata
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+        Log.d("SplashActivity", "onCreate")
+
+      Handler().postDelayed({
+          startActivity(Intent(this, LoginActivity::class.java))
+      }, 3000)
+    }
+}
