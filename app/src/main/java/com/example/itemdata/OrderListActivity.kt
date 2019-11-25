2294de4e20044ea751822259b3df5c6f826221a2
ASUS <madubashena89@gmail.com>
2019-11-24 19:46:30 -0600

    Order List

---
+++ app/src/main/java/com/example/itemdata/OrderListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.itemdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_list)
+    }
+}
