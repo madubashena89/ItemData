9faa02594bfe46a6d30b71ade7eb1fad7a5caf2a
ASUS <madubashena89@gmail.com>
2019-11-30 23:24:05 -0600

    Adding radio buttons

--- app/src/main/java/com/example/itemdata/Adaptors/OrderListAdapter.kt
+++ app/src/main/java/com/example/itemdata/Adaptors/OrderListAdapter.kt
@@ -3,11 +3,18 @@ package com.example.itemdata.Adaptors
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itemdata.Adaptors.OrderListAdapter.*
 import com.example.itemdata.R
 import com.example.itemdata.model.OrderListData
+import com.example.itemdata.model.OrderStatusEnum
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.w3c.dom.Text
 
 class OrderListAdapter(val mList: List<OrderListData>): RecyclerView.Adapter<OrderListAdapter.ViewHolder>(){
@@ -26,6 +33,16 @@ class OrderListAdapter(val mList: List<OrderListData>): RecyclerView.Adapter<Ord
         holder.tvAddress.text = data.address
         holder.tvNumber.text = data.number
         holder.tvPaymentMode.text = data.modeOfpayment
+
+        when(data.orderStatus){
+//            OrderStatusEnum.Cancelled -> holder.rg[0]
+//            OrderStatusEnum.Pending -> holder.rg[1]
+//            OrderStatusEnum.Delivered -> holder.rg[2]
+
+            OrderStatusEnum.Cancelled -> holder.rgCancel
+            OrderStatusEnum.Pending -> holder.rgPending
+            OrderStatusEnum.Delivered -> holder.rgDeliverd
+        }
     }
 
 
@@ -34,12 +51,20 @@ class OrderListAdapter(val mList: List<OrderListData>): RecyclerView.Adapter<Ord
         val tvAddress : TextView
         val tvNumber : TextView
         val tvPaymentMode : TextView
+        val rg : RadioGroup
+        val rgCancel : RadioButton
+        val rgPending : RadioButton
+        val rgDeliverd : RadioButton
 
         init {
             tvorderID = v.findViewById(R.id.tvOrder)
             tvAddress = v.findViewById(R.id.tvAddress)
             tvNumber = v.findViewById(R.id.tvMobile)
             tvPaymentMode = v.findViewById(R.id.tvPaymentMode)
+            rg = v.findViewById(R.id.rg)
+            rgCancel = v.findViewById(R.id.rg_cancel)
+            rgPending = v.findViewById(R.id.rg_pending)
+            rgDeliverd = v.findViewById(R.id.rg_delivered)
         }
 
     }
