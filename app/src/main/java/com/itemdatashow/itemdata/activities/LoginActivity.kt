d5f235840a2dc603ad67c0c9afbd898c664d2434
ASUS <madubashena89@gmail.com>
2019-12-29 16:18:52 -0600

    Google volley API requests

--- app/src/main/java/com/itemdatashow/itemdata/activities/LoginActivity.kt
+++ app/src/main/java/com/itemdatashow/itemdata/activities/LoginActivity.kt
@@ -3,11 +3,20 @@ package com.itemdatashow.itemdata.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowManager
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import com.itemdatashow.itemdata.R
+import com.itemdatashow.itemdata.util.MySingleton
 import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.reflect.Method
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+
+    val login_url = "http://dev.maxmobility.in:81/grabloyalty/qc/public/api/v1/customer/login"
+    val user_url = "https://reqres.in/api/users?page=2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +24,59 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_btn.setOnClickListener(){
-            startActivity(Intent(this, OrderListActivity::class.java))
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            tlemail.error = ""
+            tlemail.isErrorEnabled = false
+            tlPassword.error = ""
+            tlPassword.isErrorEnabled = false
+
+            if (email.isEmpty()){
+                tlemail.isErrorEnabled = true
+                tlemail.error = "Field can not be empty"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                tlemail.isErrorEnabled = true
+                tlemail.error = "Field can not be empty"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty()){
+                tlPassword.isErrorEnabled = true
+                tlPassword.error = "Field can not be empty"
+                etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            login(email, password)
+
+            //startActivity(Intent(this, OrderListActivity::class.java))
         }
+    }
+
+    private fun login(email: String, password: String) {
+        val request = object:
+        StringRequest(Method.POST, user_url,
+            Response.Listener { response ->
+
+            },
+            Response.ErrorListener { error ->
+
+            }){
+            override fun getParams(): MutableMap<String, String> {
+                val map = HashMap<String, String>()
+                map.put("username", email)
+                map.put("password", password)
+                map.put("device_type", "2")
+                return map
+
+            }
+        }
+
+        //MySingleton.getInstance()
     }
 }
