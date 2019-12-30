579c8fdfe46bb8d4757c4da4fe1bdc0f74e3e945
ASUS <madubashena89@gmail.com>
2019-12-29 20:52:12 -0600

    Google Volley

--- app/src/main/java/com/itemdatashow/itemdata/activities/LoginActivity.kt
+++ app/src/main/java/com/itemdatashow/itemdata/activities/LoginActivity.kt
@@ -1,15 +1,20 @@
 package com.itemdatashow.itemdata.activities
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.WindowManager
+import android.widget.Toast
+import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.itemdatashow.itemdata.R
-import com.itemdatashow.itemdata.util.MySingleton
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 import java.lang.reflect.Method
 import java.util.regex.Pattern
 
@@ -17,6 +22,7 @@ class LoginActivity : AppCompatActivity() {
 
     val login_url = "http://dev.maxmobility.in:81/grabloyalty/qc/public/api/v1/customer/login"
     val user_url = "https://reqres.in/api/users?page=2"
+    val jsonobj = JSONObject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +58,20 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            login(email, password)
+           // login(email, password)
+
+            jsonobj.put("username", email)
+            jsonobj.put("password", password)
+
+            val que = Volley.newRequestQueue(this@LoginActivity)
+            val req = JsonObjectRequest(Request.Method.GET,user_url, jsonobj,
+                Response.Listener {
+                    response -> Toast(this)
+
+                }, Response.ErrorListener {error ->
+                      Toast(this)
+                } )
+            que.add(req)
 
             //startActivity(Intent(this, OrderListActivity::class.java))
         }
@@ -77,6 +96,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        //MySingleton.getInstance()
+        //MySingleton.addToRequestQueue()
+       // Application.instance!!.addToReuestQueue()
     }
 }
